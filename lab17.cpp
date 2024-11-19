fa8a2798958342659d92a80c19397c31d6dcefbf
Caden See <cadensee@umich.edu>
2024-11-19 13:58:43 -0500

    att 1

--- lab17.cpp
+++ lab17.cpp
@@ -1,1 +1,11 @@
 // Definition of findHigh function goes here
+template <typename A, typename C>
+    void findHigh(A arr[], int size, int &index , C &highnum) {
+    highnum = arr[0];
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > highnum) {
+            index = i;
+            highnum = arr[i];
+        }
+    }
+}
